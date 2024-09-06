fadbaea264d8224b345db36d62452650ab301996
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-09-06 13:54:02 -0400

    Flocking Alignment ready

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -7,6 +7,14 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
 
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
+  float boidCount = 1;
+  averageVelocity += boid->getVelocity();
+
+  for (Boid* _boid : neighborhood){
+    averageVelocity = averageVelocity + _boid->getVelocity();
+    boidCount++;
+  }
+  averageVelocity = averageVelocity / boidCount;
 
   return Vector2f::normalized(averageVelocity);
 }
