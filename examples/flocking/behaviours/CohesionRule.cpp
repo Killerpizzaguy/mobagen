f90b7b00da65db7da3d28ce8ed560784acd5ba20
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-09-06 14:13:18 -0400

    Cohesion Done

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -6,8 +6,17 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
-
-  // find center of mass
+  Vector2f centerMass = boid->getPosition();
+  int boidCount= 1;
+  for(Boid* _boid : neighborhood) {
+    centerMass += _boid->getPosition();
+    boidCount++;
+  }
+  centerMass /= boidCount; //Finds center mass of the group
+  Vector2f vecToCenter = centerMass - boid->getPosition();
+  if(vecToCenter.getMagnitude() <= boid->getDetectionRadius()) {
+    cohesionForce = vecToCenter / boid->getDetectionRadius();
+  }
 
   return cohesionForce;
 }
