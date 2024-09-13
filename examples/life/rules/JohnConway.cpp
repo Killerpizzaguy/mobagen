67bc4ab986bb2ed466e35f215345c0ca3a13508c
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-09-13 14:03:01 -0400

    CountNeighbors function implemented

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -7,5 +7,14 @@ void JohnConway::Step(World& world) {
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  int livingNeighbors = 0;
+  for (int i=-1; i<=1; i++) {
+    for (int j=-1; j<=1; j++) {
+      Point2D testPoint(point.x+i, point.y+j);
+      if(testPoint != point && world.Get(testPoint)) {
+        livingNeighbors++;
+      }
+    }
+  }
+  return livingNeighbors;
 }
