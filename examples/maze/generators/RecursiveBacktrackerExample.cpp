b5e4950f2ef1c81b04ebbb282a38f5f66e03103b
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-11-29 13:27:33 -0500

    Done with recursive backtracking assignment

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,12 +4,70 @@
 #include <climits>
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
+  auto sideOver2 = w->GetSize() / 2; //copied from the functions below, I think it gets the row/column count?
+  //If it just started, add the top left cell to the main stack
+  if (!started) {
+    started = true;
+    stack.push_back(Point2D(-sideOver2, -sideOver2));
+  }
+
+  //if main stack isn't empty
+  if (!stack.empty()) {
+    Point2D mainPoint = stack.back();
+    std::vector<Point2D> neighbours = getVisitables(w, mainPoint);
+    Point2D chosenNeighbour;
+
+    //mark the point as visited
+    visited[mainPoint.y][mainPoint.x] = true;
+    w->SetNodeColor(mainPoint, Color::Blue);
+
+    //if there are visitable neighbors
+    if (neighbours.size() > 0) {
+      //get all visitable neighbors
+
+
+      if (neighbours.size() == 1) {
+        chosenNeighbour = neighbours.back();
+      }
+
+      //rng a neighbor
+      else {
+        chosenNeighbour = neighbours[getFakeRandomNumber() % neighbours.size()];
+      }
+
+      //remove the wall between current cell and the neighbor
+      if (chosenNeighbour.y < mainPoint.y)
+        w->SetNorth(mainPoint, false);
+      if (chosenNeighbour.x > mainPoint.x)
+        w->SetEast(mainPoint, false);
+      if (chosenNeighbour.y > mainPoint.y)
+        w->SetSouth(mainPoint, false);
+      if (chosenNeighbour.x < mainPoint.x)
+        w->SetWest(mainPoint, false);
+
+      //add neighbor to the stack
+      stack.push_back(chosenNeighbour);
+      return true;
+    }
+
+    //if there aren't visitable neighbors
+    else {
+      w->SetNodeColor(mainPoint, Color::Green);
+      stack.erase(stack.end());
+      return true;
+    }
+  }
+
+
+
+
   return false;
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
   visited.clear();
   stack.clear();
+  started = false;
   auto sideOver2 = world->GetSize() / 2;
 
   for (int i = -sideOver2; i <= sideOver2; i++) {
@@ -34,6 +92,45 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   std::vector<Point2D> visitables;
 
   // todo: implement this
+  //top
+  Point2D top (p.x, p.y - 1);
+  if (checkVisitable(top, sideOver2)) {
+    visitables.push_back(top);
+  }
+  //right
+  Point2D right(p.x + 1, p.y);
+  if (checkVisitable(right, sideOver2)) {
+    visitables.push_back(right);
+  }
+  //bottom
+  Point2D bottom (p.x, p.y + 1);
+  if (checkVisitable(bottom, sideOver2)) {
+    visitables.push_back(bottom);
+  }
+  //left
+  Point2D left(p.x - 1, p.y);
+  if (checkVisitable(left, sideOver2)) {
+    visitables.push_back(left);
+  }
+  std::cout<<"visitables size: "<<visitables.size()<<std::endl;
 
   return visitables;
 }
+
+bool RecursiveBacktrackerExample::checkVisitable(Point2D p, int sideOver2) {
+  if (p.y >= -sideOver2 && p.y <= sideOver2 && p.x >= -sideOver2 && p.x <= sideOver2) {
+    return !visited[p.y][p.x];
+  }
+  return false;
+}
+
+
+int RecursiveBacktrackerExample::getFakeRandomNumber() { //function to get from the fixed set of random values that the assignment said to use
+  int rand = FAKE_RANDOM_ARRAY[fakeRandomIndex];
+  fakeRandomIndex++;
+  if (fakeRandomIndex > FAKE_RANDOM_SIZE-1) {
+    fakeRandomIndex = 0;
+  }
+  return rand;
+}
+
--- examples/maze/generators/RecursiveBacktrackerExample.h
+++ examples/maze/generators/RecursiveBacktrackerExample.h
@@ -11,7 +11,13 @@ private:
   std::vector<Point2D> stack;
   std::map<int, std::map<int, bool>> visited;  // naive. not optimal
   Point2D randomStartPoint(World* world);
+  int getFakeRandomNumber();
   std::vector<Point2D> getVisitables(World* w, const Point2D& p);
+  bool checkVisitable(Point2D p, int sideOver2);
+  bool started = false;
+  const int FAKE_RANDOM_SIZE = 100;
+  const int FAKE_RANDOM_ARRAY[100] ={72, 99, 56, 34, 43, 62, 31, 4, 70, 22, 6, 65, 96, 71, 29, 9, 98, 41, 90, 7, 30, 3, 97, 49, 63, 88, 47, 82, 91, 54, 74, 2, 86, 14, 58, 35, 89, 11, 10, 60, 28, 21, 52, 50, 55, 69, 76, 94, 23, 66, 15, 57, 44, 18, 67, 5, 24, 33, 77, 53, 51, 59, 20, 42, 80, 61, 1, 0, 38, 64, 45, 92, 46, 79, 93, 95, 37, 40, 83, 13, 12, 78, 75, 73, 84, 81, 8, 32, 27, 19, 87, 85, 16, 25, 17, 68, 26, 39, 48, 36};
+  int fakeRandomIndex = 0;
 
 public:
   RecursiveBacktrackerExample() = default;
