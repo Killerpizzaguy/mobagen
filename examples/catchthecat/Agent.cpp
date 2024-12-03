9f3a673b5562c25e00af62f0800dbc92cf5fd3ed
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-12-03 12:04:23 -0500

    Done with generatePath

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -17,41 +17,36 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
   bool borderFound = false;
 
-  while (!frontier.empty()) {
+  while (!frontier.empty()){
     // get the current from frontier
     Point2D current = frontier.front();
-    //might need to do frontier.pop();
+    frontier.pop();
     // remove the current from frontierset
     frontierSet.erase(current);
     // mark current as visited
-    visited[current] = false;
+    visited.insert({current, true});
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
     std::vector<Point2D> neighbors;
 
     for (auto neighbor : w->neighbors(current)) {
-      if (!w->getContent(neighbor) && !visited[neighbor]) { //Checks if the neighbor is empty and hasn't been visited yet
-        bool valid = true;
-        for (auto point : frontier) { //makes sure the neighbor isn't already in the frontier
-            if(point == neighbor) {
-              valid = false;
-              break;
-            }
-          }
-        if(valid) {
+      std::cout<<"checking point "<< neighbor.x << " ," << neighbor.y << std::endl;
+      if (!w->getContent(neighbor) && !frontierSet.contains(neighbor)) { //Checks if the neighbor is empty and isn't in the frontierSet
+        if(!visited.contains(neighbor) || (visited.contains(neighbor) && visited.at(neighbor)) == false) {
           neighbors.push_back(neighbor);
+          std::cout<<"valid neighbor found"<<std::endl;
         }
       }
     }
-
+    std::cout<<"Valid neighbors found = "<< neighbors.size() << std::endl;
     // iterate over the neighs:
     for (auto neighbor : neighbors) {
       // for every neighbor set the cameFrom
-      cameFrom[neighbor] = current;
+      cameFrom.insert({neighbor, current});
       // enqueue the neighbors to frontier and frontierset
       frontier.push(neighbor);
       frontierSet.insert(neighbor);
       // do this up to find a visitable border and break the loop
-      if(!w->isValidPosition(neighbor)) {
+      if(w->catWinsOnSpace(neighbor)) {
         borderFound = true;
         borderExit = neighbor; //should this be current or neighbor?
         break;
@@ -65,6 +60,24 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   }
 
   // if the border is not infinity, build the path from border to the cat using the camefrom map
+  if(borderExit != Point2D::INFINITE) {
+    std::vector<Point2D> path;
+    Point2D p = borderExit;
+    while(true) {
+      path.push_back(p);
+      if(cameFrom.contains(p)) {
+        p = cameFrom[p];
+        if(p == catPos) {
+          break;
+        }
+      }
+      else {
+        break;
+      }
+    }
+
+    return path;
+  }
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
   return vector<Point2D>();
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -2,10 +2,14 @@
 #include "World.h"
 
 Point2D Catcher::Move(World* world) {
-  auto side = world->getWorldSideSize() / 2;
-  for (;;) {
-    Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
-    auto cat = world->getCat();
-    if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
-  }
+  // auto side = world->getWorldSideSize() / 2;
+  // for (;;) {
+  //   Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
+  //   auto cat = world->getCat();
+  //   if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
+  //}
+
+  std::vector<Point2D> path = generatePath(world);
+  std::cout<<"Catcher target = " << path.front().x << " ," << path.front().y << std::endl;
+  return path.front();
 }
