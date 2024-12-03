862b2c89698e6c1cdd4e66e30c65599985db3d32
Will Rutherford <william.rutherford@mymail.champlain.edu>
2024-12-03 10:54:28 -0500

    Half done with agent.cpp

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -15,16 +15,53 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   frontier.push(catPos);
   frontierSet.insert(catPos);
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
+  bool borderFound = false;
 
   while (!frontier.empty()) {
     // get the current from frontier
+    Point2D current = frontier.front();
+    //might need to do frontier.pop();
     // remove the current from frontierset
+    frontierSet.erase(current);
     // mark current as visited
+    visited[current] = false;
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    std::vector<Point2D> neighbors;
+
+    for (auto neighbor : w->neighbors(current)) {
+      if (!w->getContent(neighbor) && !visited[neighbor]) { //Checks if the neighbor is empty and hasn't been visited yet
+        bool valid = true;
+        for (auto point : frontier) { //makes sure the neighbor isn't already in the frontier
+            if(point == neighbor) {
+              valid = false;
+              break;
+            }
+          }
+        if(valid) {
+          neighbors.push_back(neighbor);
+        }
+      }
+    }
+
     // iterate over the neighs:
-    // for every neighbor set the cameFrom
-    // enqueue the neighbors to frontier and frontierset
-    // do this up to find a visitable border and break the loop
+    for (auto neighbor : neighbors) {
+      // for every neighbor set the cameFrom
+      cameFrom[neighbor] = current;
+      // enqueue the neighbors to frontier and frontierset
+      frontier.push(neighbor);
+      frontierSet.insert(neighbor);
+      // do this up to find a visitable border and break the loop
+      if(!w->isValidPosition(neighbor)) {
+        borderFound = true;
+        borderExit = neighbor; //should this be current or neighbor?
+        break;
+      }
+    }
+
+  if(borderFound) {
+    break;
+  }
+
   }
 
   // if the border is not infinity, build the path from border to the cat using the camefrom map
